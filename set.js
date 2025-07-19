d4e928765b923187c1708be63ca765f0a3625aa1
beltah <isaac0maina@gmail.com>
2025-07-19 09:42:53 +0300

    Update set.js

--- set.js
+++ set.js
@@ -6,7 +6,7 @@ There's no need to configure this if you're deploying via Heroku — just set th
 const sessionName = 'session';
 
 // ✅ Correct MEGA session format (URL encoded)
-const session = 'BLACK MD;;;BnsGUIxT#IJ84hOY4kQPGvGjoTWXRM9OmMZP7xtpmfCWFJ9FvyQc'; // This is OK as-is
+const session = 'BLACK MD;;;V7kHjLAK#EPGYYnmMhploIZJakUPoo8FncFDNxwTLJwpfaGTaMN0'; // This is OK as-is
 
 const autobio = process.env.AUTOBIO || 'FALSE';
 const autolike = process.env.AUTOLIKE_STATUS || 'TRUE';
